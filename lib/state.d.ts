0c6a2adeeefd417267e192278dd0b3da6a585e48
Jacob Schneider <jakieschneider13@gmail.com>
2022-06-10 03:15:45 +0000

    Added StateManager and MessageChannel modules

---
+++ lib/state.d.ts
@@ -0,0 +1,19 @@
+declare module "jcake-utils/state" {
+    export default class StateManager<State extends {}> {
+        private readonly globState;
+        private readonly watchers;
+        private readonly eventHandlers;
+        constructor(defaults?: Partial<State>);
+        private invokeHandlers;
+        get(): State;
+        setState(state?: Partial<State> | ((prev: State) => Partial<State>)): State;
+        setStateAsync(state?: Promise<Partial<State>> | ((prev: State) => Promise<Partial<State>>)): Promise<State>;
+        onStateChange(callback: (state: State) => any): number;
+        removeStateListener(id: number): void;
+        on(event: string, callback: (state: State) => any): number;
+        off(id: number): void;
+        dispatch(event: string, state: Partial<State> | ((prev: State) => Partial<State>)): void;
+        broadcast(event: string): void;
+    }
+    
+}
