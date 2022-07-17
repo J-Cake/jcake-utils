3683f92667f7fd5a25f88dc9d7377b464907c15f
J-Cake <jakieschneider13@gmail.com>
2022-07-17 20:12:27 +1000

    Parser API defined

--- src/parse.ts
+++ src/parse.ts
@@ -1,12 +1,23 @@
 export * as Lex from './lex';
 
-export interface ParserBuilder<T extends string> {
-    exactly(): ParserBuilder<T>,
-    oneOf(): ParserBuilder<T>,
-    repeat(): ParserBuilder<T>,
-    maybe(): ParserBuilder<T>
+export type ASTNode<T extends string, K extends string> = {
+    type: K,
+    tokens: Token<T>[]
 }
 
-export function createParser<T extends string>(name: string): ParserBuilder<T> {
+export interface ParserBuilder<T extends string, K extends string> {
+    exactly(...parser: ParserBuilder<T>): ParserBuilder<T>,
+    oneOf(...parser: ParserBuilder<T>):   ParserBuilder<T>,
+    repeat(...parser: ParserBuilder<T>):  ParserBuilder<T>,
+    maybe(...parser: ParserBuilder<T>):   ParserBuilder<T>,
 
+    exec(tokens: AsyncIterable<Token<T>>): Promise<ASTNode<T, K>>
+}
+
+export function createParser<T extends string, K extends string>(name: K): ParserBuilder<T> {
+    const parser: ParserBuilder<T, K> = {
+
+    };
+
+    return parser;
 }
