62f02c56da57053055f4d0ec32c8608360dcfb8e
J-Cake <jakieschneider13@gmail.com>
2022-07-30 11:31:25 +1000

    Do not make any reference to external dependencies in build

--- test/test/msg.js
+++
@@ -1,5 +0,0 @@
-// src/msgChannel.ts
-import assert from "node:assert";
-import msgChannel from "@j-cake/jcake-utils/msg";
-assert.equal(typeof msgChannel, "function");
-//# sourceMappingURL=msg.js.map
